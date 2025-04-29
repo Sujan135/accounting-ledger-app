8c107325916d3db18c62a5a15d71f0c639fadaff
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-29 14:15:15 -0500

    Implemented Ledger class with menu options for viewing all transactions, deposits, payments, and navigating back to home

--- AccountingLedgerApp/src/main/java/com/pluralsight/Ledger.java
+++ AccountingLedgerApp/src/main/java/com/pluralsight/Ledger.java
@@ -17,6 +17,7 @@ public class Ledger {
             System.out.println("A) All");
             System.out.println("D) Deposits");
             System.out.println("P) Payments");
+            System.out.println("R) Reports");
             System.out.println("H) Home");
 
             String choice = scanner.nextLine().toUpperCase();
@@ -26,11 +27,15 @@ public class Ledger {
                     displayAll();
                     break;
                 case "D":
+                    displayDeposits();
                     break;
                 case "P":
+                    displayPayments();
+                    break;
+                case "R":
                     break;
                 case "H":
-                    break;
+                    return;
                 default:
                     System.out.println("Invalid option. Try again.");
             }
@@ -41,6 +46,25 @@ public class Ledger {
         System.out.println("All Transactions:");
         for (int i = transactions.size()-1; i >= 0; i--){
             System.out.println(transactions.get(i));
+        }
+    }
+
+    private void displayDeposits() {
+        System.out.println("Deposits Only");
+        for (int i = transactions.size() -1 ; i >= 0; i--) {
+            if (transactions.get(i).getAmount() > 0) {
+                System.out.println(transactions.get(i));
+            }
+        }
+    }
+
+    private void displayPayments() {
+        System.out.println("Payments Only:");
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            if (transactions.get(i).getAmount() < 0) {
+                System.out.println(transactions.get(i));
+
+            }
         }
     }
 
